eff94c2dd02a94c55b41b5d2b122ec6fece3b534
Abdullah Ali Mohamed <Abdallah2ali2020@gmail.com>
2023-09-04 14:20:44 +0300

    ttttt

--- HelloITI.java
+++ HelloITI.java
@@ -2,7 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello ITI");
         System.out.println("Hello GIT");
-        System.out.println(args[50]);
+        System.out.println(args[30]);
 		System.out.println(args[1]);
     }
 }
