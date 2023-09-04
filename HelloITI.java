425238d5a7dace4fd924e0582673b2064525ebca
Omar El Samahy <eng.omaryasser@gmail.com>
2023-09-04 14:18:03 +0300

    Args Change

--- HelloITI.java
+++ HelloITI.java
@@ -2,7 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello ITI");
         System.out.println("Hello GIT");
-        System.out.println(args[50]);
+        System.out.println(args[20]);
 		System.out.println(args[1]);
     }
 }
