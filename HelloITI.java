bea4f8b0b4dc63ecdf50cf610266e9503ca6a7cd
Omar El Samahy <eng.omaryasser@gmail.com>
2023-09-04 13:16:40 +0300

    Initial commit

---
+++ HelloITI.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello ITI");
+        System.out.println("Hello GIT");
+        System.out.println(args[0]);
+    }
+}
---
+++ HelloSwift.java
@@ -0,0 +1,8 @@
+class HelloSwift
+{
+	public static void main (String[] args)
+	{
+		System.out.println ("Hello World");
+		System.out.println (args[0]);
+		}
+}
